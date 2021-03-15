5363f0127175334db60c91e3197cd08b192aac72
DESKTOP-9RAS77D\20929 <20191548@g.dongseo.ac.kr>
2021-03-15 23:11:57 +0900

    컴퓨터 개론:토픽1[실습과제]

---
+++ 변수 실습과제.py
@@ -0,0 +1,24 @@
+#칼로리 계산
+
+#총 다섯 가지 과자가 있습니다.
+'''
+kitkat: 190 칼로리
+oreos: 502 칼로리
+pringles: 292 칼로리
+twix: 135.9 칼로리
+cheetos: 485 칼로리
+'''
+#과자를 다양하게 조합해서 먹었을 때 총 몇 칼로리인지 계산해 보려고 하는데요.
+#각 과자의 이름을 변수 이름으로 사용하여, 해당 과자의 칼로리를 저장해 주세요.
+
+kitkat = 190
+oreos = 502
+cheetos = 485
+pringles = 292
+twix = 135.9
+
+# 다양한 과자 조합
+print(kitkat + oreos * 2)
+print(cheetos * 4)
+print(pringles + oreos + twix)
+print(pringles * 3 + oreos * 2)
---
+++ 피라미터 실습과제.py
@@ -0,0 +1,10 @@
+# 세 수의 곱을 알려주는 프로그램
+# 파라미터로 정수 값 세 개를 받고, 세 수의 곱을 출력하는 함수 multiply_three_numbers를 만들어보세요
+def multiply_three_numbers(a, b, c):
+    print(a*b*c)
+
+
+# 테스트 코드
+multiply_three_numbers(7, 3, 5)
+multiply_three_numbers(21, 4, 9)
+multiply_three_numbers(-7, 6, 3)
