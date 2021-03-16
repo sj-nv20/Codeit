20fa0d7278cf4b7c8645d6d404352409baf47a66
DESKTOP-9RAS77D\20929 <20191548@g.dongseo.ac.kr>
2021-03-16 23:49:35 +0900

    자료형 - 문자열

---
+++ 자료형 - 문자열.py
@@ -0,0 +1,15 @@
+# 자료형
+
+# 2. 문자열 : 키보드로 쓸 수 있는 문자들을 표현하는 자료형
+print('원하는 문자열 입력')     #원하는 문자열 입력
+print("원하는 문자열 입력")     #원하는 문자열 입력
+#'으로 시작하면'으로 끝내고 "로 시작하면 "로 끝내기
+print("i', excited to learn Python')    #오류
+#문자열을 출력할때는 "가 나타나지 않음
+#출력할 때 "가 나타나게 하고 싶으면 \" , \'로 표현해주기
+print("I\'m \"excited\" to learn Python!")  # I'm "excited" to learn Python!
+
+#문자열 덧셈 => 문자열 연결
+print("Hello" * 3)  #HelloHelloHello
+print("Hello" + "World")    #HelloWorld
+print("3" + "5")    #정수가 아닌 두 문자열이 연결되는 것 -> 35출력
